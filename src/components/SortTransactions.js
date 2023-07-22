fd677710ce0c513db4931a386b9ffb7976fe6a1a
Ian Raburu <williamraburu@gmail.com>
2023-07-22 04:38:14 +0300

    Set component's function

---
+++ src/components/SortTransactions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SortTransactions() {
+    
+}
+
+export default SortTransactions;
---
+++ src/components/TransactionList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Transaction from "./Transaction";
+
+function TransactionList() {
+    
+}
+
+export default TransactionList;
