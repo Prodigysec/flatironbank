b94730ed715179a314ea9e1c4c34d04f3a425878
Ian Raburu <williamraburu@gmail.com>
2023-07-22 14:40:11 +0300

    Add transaction details

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-function transaction() {
-    
+function Transaction({ transactions }) {
+    return (
+        transactions.map((transaction, index) => {
+            return (
+                <div className="transaction" key={index}>
+                    <p>Date: {transaction.date}</p>
+                    <p>Description: {transaction.description}</p>
+                    <p>Category: {transaction.category}</p>
+                    <p>Amount: {transaction.amount}</p>
+                </div>)
+        })
+
+    )
 }
 
-export default transaction;
+export default Transaction;
