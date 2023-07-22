22de3dfa72e5557f6159d4244602e42316821d69
Ian Raburu <williamraburu@gmail.com>
2023-07-22 03:58:09 +0300

    Fetch data from transactions endpoint in localhost

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TransactionList from "./components/TransactionList";
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/transactions`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTransactions(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       
