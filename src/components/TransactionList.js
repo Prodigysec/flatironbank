b5022e328a0a17a753aa017d51babd352878fd55
Ian Raburu <williamraburu@gmail.com>
2023-07-22 14:47:34 +0300

    Add transaction to transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionList() {
-    
+function TransactionList({ transactions }) {
+    return (
+        <div className="transactions">
+            <Transaction transactions={transactions} />
+        </div>
+    );
 }
 
 export default TransactionList;
