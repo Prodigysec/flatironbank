f17d2c310fd2897a8139527ccb679d60d109df51
Ian Raburu <williamraburu@gmail.com>
2023-07-22 16:05:23 +0300

    Add a state for user input and a function to handle change in user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchBar() {
-    
+function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
 }
 
 export default SearchBar;
