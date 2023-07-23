22090290298eb1eaca6de0fe6703de8c4e8219e7
Ian Raburu <williamraburu@gmail.com>
2023-07-23 21:50:04 +0300

    Add a function to delete transaction

--- src/components/DeleteTransaction.js
+++ src/components/DeleteTransaction.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function DeleteTransaction() {
-    
+function DeleteTransaction({ transactionId, onDelete }) {
+    const handleDelete = () => {
+        onDelete(transactionId);
+    };
+
+    return (
+        <button onClick={handleDelete} className="delete-button">
+            Delete transaction
+        </button>
+    );
 }
 
 export default DeleteTransaction;
